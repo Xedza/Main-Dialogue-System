21ddc693dfd44b3decb68c64748e605127fa0390
Xedza <277286108+Xedza@users.noreply.github.com>
2026-04-20 08:55:29 -0700

    Create main.cpp
    Unfinished.

---
+++ main.cpp
@@ -0,0 +1,76 @@
+#include <iostream>
+#include <string>
+#include <cstdlib>
+#include <chrono>
+#include <thread>
+
+
+// Player Class
+class Player {
+    public:
+        int health = 100;
+        int stamina = 100;
+        bool gender = false;
+};
+
+
+// Other NPC class
+class Tom {
+    public:
+        int relationship = 0;
+};
+
+class Ana {
+    public:
+        int relationship = 0;
+
+};
+// Simplifications
+Player plr;
+Ana a;
+Tom t;
+
+// Strings
+std::string playerName;
+
+// Int
+
+// Bools
+
+
+int main() {
+    system("clear");
+    std::cout << "T: What's up man! You seem new to this school. What's your name?\n";
+    std::cin >> playerName;
+    std::cout << "T: Nice to meet you, " << playerName << "! Want me to show you around school?\n" << std::endl;
+    std::this_thread::sleep_for(std::chrono::seconds(1));
+    std::cout << "\n";
+    std::cout << "Follow tom? (yes/no)\n";
+    std::string followTom;
+    std::cin >> followTom;
+
+    if (followTom == "yes") {
+        std::cout << playerName << ": Sure, lead the way!\n" << std::endl;
+        t.relationship += 20;
+        std::this_thread::sleep_for(std::chrono::seconds(1));
+        std::cout << "T: Sure! follow me\n" << std::endl;
+        std::this_thread::sleep_for(std::chrono::seconds(1));
+        std::cout << "T: Your relationship with Tom is now " << t.relationship << ".\n" << std::endl;
+        std::this_thread::sleep_for(std::chrono::seconds(2));
+        system("clear");
+    }
+    if (followTom == "no") {
+        std::cout << playerName << ": Nah, I think I'll explore on my own.\n" << std::endl;
+        t.relationship -= 20;
+        std::this_thread::sleep_for(std::chrono::seconds(1));
+        std::cout << "T: Oh, okay. Suit yourself.\n" << std::endl;
+        std::this_thread::sleep_for(std::chrono::seconds(1));
+        std::cout << "T: Your relationship with Tom is now " << t.relationship << ".\n" << std::endl;
+        std::this_thread::sleep_for(std::chrono::seconds(2));
+        system("clear");
+    }  else {
+        std::cout << "Put a valid command and make sure it is lowercase!" << std::endl;
+    }
+
+    return 0;
+}
