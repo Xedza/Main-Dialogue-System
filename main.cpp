80e121f01f1b596e519e0e694c629f59087699a7
Xedza <277286108+Xedza@users.noreply.github.com>
2026-04-20 13:26:31 -0700

    Fixed some errors

--- main.cpp
+++ main.cpp
@@ -4,7 +4,39 @@
 #include <chrono>
 #include <thread>
 #include "container.h"
-#include "scenes.h"
+
+// Player Class
+class Player {
+    public:
+        int health = 100;
+        int stamina = 100;
+};
+
+
+// Other NPC class
+class Tom {
+    public:
+        int relationship = 0;
+};
+
+class Ana {
+    public:
+        int relationship = 0;
+
+};
+// Simplifications
+Player plr;
+Ana a;
+Tom t;
+
+// Strings
+std::string playerName;
+
+// Int
+
+// Bools
+bool followedtom = false;
+
 
 int main() {
     system("clear");
